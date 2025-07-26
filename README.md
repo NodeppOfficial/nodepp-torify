0f62d519d5ff351ea25783abfbb69cac45a6e82e
penguin <becerracenmanueld@gmail.com>
2025-07-26 08:27:43 -0400

    📌 Torify | Stable Release | V1.0.0 📌

--- README.md
+++ README.md
@@ -1,10 +1,51 @@
-# NODEPP-TOR-CLIENT
+# NodePP Tor Client: Basic Onion Routing for NodePP
 
-A Simple TOR client for NodePP
+This project provides a fundamental Tor client implementation for the NodePP framework. Tor (The Onion Router) is a network that helps you defend against traffic analysis, a form of network surveillance that threatens personal freedom and privacy, confidential business activities and relationships, and state security. This library aims to allow NodePP applications to route their network traffic through the Tor network for enhanced anonymity and privacy.
+
+## Key Features
+
+- **Basic Tor Connection:** Establishes a connection to the Tor network.
+- **SOCKS5 Proxy Support:** Implements a SOCKS5 proxy client to forward traffic through Tor.
+- **Simple API:** Offers a straightforward interface to connect and send data over Tor within NodePP.
+- **Asynchronous Operations:** Designed to integrate seamlessly with NodePP's asynchronous event loop.
+
+## Usage 
+```cpp
+#include <nodepp/nodepp.h>
+#include <nodepp/url.h> 
+#include <torify/http.h>
+
+using namespace nodepp;
+
+void onMain() {
+
+    tor_fetch_t args;
+    args.timeout = 0; // Disable Fetch timeout
+    args.method  = "GET";
+    args.proxy   = "tcp://localhost:9050";
+    args.url     = "http://check.torproject.org/";
+    args.headers = header_t({
+        { "Host", url::hostname( args.url ) }
+    });
+
+    tor::http::fetch( args )
+
+    .then([]( http_t cli ){
+        stream::pipe( cli, fs::std_output() );
+    })
+
+    .fail([]( except_t err ){
+        console::log(err);
+    });
+
+}
+```
 
 ## Build & Run
-- 🐧: `g++ -o main main.cpp -I ./include ; ./main`
-- 🪟: `g++ -o main main.cpp -I ./include -lws2_32 ; ./main`
+```bash
+🪟: g++ -o main main.cpp -I ./include -lws2_32 ; ./main
+🐧: g++ -o main main.cpp -I ./include ; ./main
+```
 
 ## License
 
---
+++ include/torify/tcp.h
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2023 The Nodepp Project Authors. All Rights Reserved.
+ *
+ * Licensed under the MIT (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://github.com/NodeppOficial/nodepp/blob/main/LICENSE
+ */
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+#ifndef NODEPP_TORIFY_TCP
+#define NODEPP_TORIFY_TCP
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+#include <nodepp/tcp.h>
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+#ifndef NODEPP_TOR_AGENT_T
+#define NODEPP_TOR_AGENT_T
+namespace nodepp { struct torify_agent_t : public agent_t {
+    string_t proxy = "tcp://localhost:9050";
+};}
+#endif
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+namespace nodepp {
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+class tcp_torify_t {
+private:
+
+    using CALLBACK = function_t<void,socket_t>;
+
+protected:
+
+    struct NODE {
+        int            state = 0;
+        int            accept=-2;
+        torify_agent_t agent;
+        CALLBACK       func ;
+    };  ptr_t<NODE> obj;
+
+    ptr_t<char> htons( uint16 host_short ) const noexcept {
+        ptr_t<char> tmp( 3, '\0' ); if( host_short >= 255 ) {
+            tmp[0] = (char)( host_short >> 8 );
+            tmp[1] = (char)( host_short >> 0 );
+        } else {
+            tmp[1] = (char)( host_short );
+        } return tmp;
+    }
+
+public: tcp_torify_t() noexcept : obj( new NODE() ) {}
+
+    event_t<socket_t> onConnect;
+    event_t<socket_t> onSocket;
+    event_t<>         onClose;
+    event_t<except_t> onError;
+    event_t<socket_t> onOpen;
+
+    /*─······································································─*/
+
+   ~tcp_torify_t() noexcept { if( obj.count() > 1 ){ return; } free(); }
+
+    tcp_torify_t( CALLBACK _func, torify_agent_t* opt=nullptr ) noexcept : obj( new NODE() ) {
+        obj->agent = (opt==nullptr) ? torify_agent_t() : *opt; obj->func = _func;
+    }
+
+    /*─······································································─*/
+
+    void     close() const noexcept { if(obj->state<=0){return;} obj->state=-1; onClose.emit(); }
+    bool is_closed() const noexcept { return obj == nullptr ? 1: obj->state<=0; }
+
+    /*─······································································─*/
+
+    void listen( const string_t& host, int port, CALLBACK cb ) const {
+         process::error( "servers aren't supported by torify" );
+    }
+
+    /*─······································································─*/
+
+    void connect( const string_t& host, int port, CALLBACK cb ) const noexcept {
+        if( obj->state == 1 ){ return; } if( dns::lookup(obj->agent.proxy).empty() )
+          { onError.emit("dns couldn't get ip"); close(); return; }
+
+        auto self = type::bind(this); auto clb = [=](){
+
+            socket_t sk; self->obj->state = 1;
+                     sk.SOCK    = SOCK_STREAM;
+                     sk.IPPROTO = IPPROTO_TCP;
+
+            if( sk.socket( dns::lookup(
+                url::hostname( self->obj->agent.proxy ) ), 
+                url::port    ( self->obj->agent.proxy ) )<0 
+            ) { self->onError.emit("Error while creating TCP"); 
+                self->close(); sk.free(); return; 
+            }   sk.set_sockopt( self->obj->agent );
+
+            process::add( coroutine::add( COROUTINE(){
+            int c=0; coBegin; coWait(!limit::fileno_ready());
+
+                coWait( (c=sk._connect())==-2 ); if( c<=0 ){
+                    self->onError.emit("Error while connecting TCP");
+                    self->close(); coEnd;
+                }
+
+                do { int len = type::cast<int>( host.size() );
+
+                    sk.write( ptr_t<char>({ 0x05, 0x01, 0x00, 0x00 }) );
+                    if( sk.read(2)!=ptr_t<char>({ 0x05, 0x00, 0x00 }) ){
+                        self->onError.emit("Error while Handshaking Sock5");
+                    coEnd; }
+
+                    sk.write( ptr_t<char>({ 0x05, 0x01, 0x00, 0x03, 0x00 }) );
+                    sk.write( ptr_t<char>({ len, 0x00 }) ); sk.write( host );
+                    sk.write( htons( port ) ); sk.read();
+
+                } while(0);
+
+                sk.onDrain.once([=](){ self->close(); }); cb(sk);
+                self->onSocket.emit(sk); self->obj->func(sk);
+
+                if( sk.is_available() ){ 
+                    sk.onOpen      .emit(  );
+                    self->onOpen   .emit(sk); 
+                    self->onConnect.emit(sk); 
+                }
+
+            coFinish }));
+
+        };
+
+        process::add( coroutine::add( COROUTINE(){
+        coBegin; coWait( !limit::fileno_ready() );
+                 clb();
+        coFinish
+        }));
+
+    }
+
+    /*─······································································─*/
+
+    void connect( const string_t& host, int port ) const noexcept {
+         connect( host, port, []( socket_t ){} );
+    }
+
+    void listen( const string_t& host, int port ) const noexcept {
+         listen( host, port, []( socket_t ){} );
+    }
+
+    /*─······································································─*/
+
+    void free() const noexcept {
+        if( is_closed() ){ return; }close();
+        onConnect.clear(); onSocket.clear();
+        onError  .clear(); onOpen  .clear();
+    }
+
+};
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+namespace torify { namespace tcp {
+
+    tcp_torify_t client( torify_agent_t* opt=nullptr ){
+        auto skt = tcp_torify_t( [=]( socket_t ){}, opt ); return skt;
+    }
+
+}}
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+}
+
+#endif
